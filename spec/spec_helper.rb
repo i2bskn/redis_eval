ac29f28d8d939876232243846d12af038c3ba7be
i2bskn <i2bskn@gmail.com>
2017-04-07 17:48:28 +0900

    update

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,6 +21,13 @@ LUA_SCRIPT_PATH = [File.expand_path("../lua_scripts", __FILE__)]
 TEST_DB = "redis://localhost:6379/10"
 
 RSpec.configure do |config|
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = ".rspec_status"
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+
   config.order = :random
   config.after(:each) {
     RedisEval.config.reset
