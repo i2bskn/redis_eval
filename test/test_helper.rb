8efac38055dfec262fb5fe088affe10f639ecd26
i2bskn <i2bskn@gmail.com>
2017-04-20 17:57:21 +0900

    coveralls

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,19 @@
 require "bundler/setup"
+require "simplecov"
+require "coveralls"
+
+Coveralls.wear!
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter::new([
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+])
+
+SimpleCov.start do
+  add_filter "spec"
+  add_filter ".bundle"
+end
+
 require "redis_eval"
 
 require "minitest/autorun"
